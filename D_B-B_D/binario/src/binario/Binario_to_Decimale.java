5421f3155dd1f6dfef5a882d595563292fad4ec6
Antoino0704 <76400595+Antoino0704@users.noreply.github.com>
2021-12-30 18:44:05 +0100

    Update Binario_to_Decimale.java

--- D_B-B_D/binario/src/binario/Binario_to_Decimale.java
+++ D_B-B_D/binario/src/binario/Binario_to_Decimale.java
@@ -2,13 +2,13 @@ package binario;
 
 public class Binario_to_Decimale {
 	private int max, risultato;
-	private String str1;
 	private String[] array;
 	
 	public Binario_to_Decimale(int numero) {
-		str1 = Integer.toString(numero);
+		String str1 = Integer.toString(numero);
 		array = str1.split("");
 		max = array.length - 1;
+		risultato = 0;
 	}
 	
 	public int Convert_D() {
