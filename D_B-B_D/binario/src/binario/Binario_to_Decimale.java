342ff56122e033eab58d8b9bd75892f3b2357cb6
Antoino0704 <76400595+Antoino0704@users.noreply.github.com>
2021-12-30 21:46:14 +0100

    Update Binario_to_Decimale.java

--- D_B-B_D/binario/src/binario/Binario_to_Decimale.java
+++ D_B-B_D/binario/src/binario/Binario_to_Decimale.java
@@ -1,23 +1,41 @@
 package binario;
 
-public class Binario_to_Decimale {
-	private int max, risultato;
+/**
+ * Convert binary number to number decimal
+ * @author Buscarino Antonino
+ * @version 1.0.1
+ *
+ */
+public class Binary_to_decimal {
+	private int index, result;
 	private String[] array;
 	
-	public Binario_to_Decimale(int numero) {
-		String str1 = Integer.toString(numero);
-		array = str1.split("");
-		max = array.length - 1;
-		risultato = 0;
+	/**
+	 * Constructor
+	 * @param num
+	 */
+	public Binary_to_decimal(int num) {
+		String str = Integer.toString(num);
+		array = str.split("");
+		index = array.length - 1;
+		result = 0;
 	}
 	
-	public int Convert_D() {
-		for(int i = 0; i<array.length; i++, max--) {
-			if(array[max].equals("1")) {
-				risultato += (Math.pow(2, i));
+	/**
+	 * this method checks which elements of the split array have value 1 and
+	 * adds the sum between result and 2 raised to the position of the element being checked
+	 *  <br>
+	 *  <b>alert if the number is negative, it returns the number converted to a positive decimal</b>
+	 * @return number convert in decimal
+	 */
+	public int convert() {
+		for(int i=0; i<array.length; i++, index--) {
+			if(array[index].equals("1")) {
+				result += (Math.pow(2, i));
 			}
 		}
-		return risultato;
+		return result;
 	}
 
 }
+
